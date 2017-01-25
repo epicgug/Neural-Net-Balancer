103d6e4518a5ba67dd5c616aed55634c4526d8c0
Justin Dachille <epicgug@users.noreply.github.com>
2017-01-24 17:07:08 -0800

    Added Scripts

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasController : MonoBehaviour {
+	public Text lastLayerText, scoreText, bestScore;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SetText ();
+	}
+
+	void SetText() {
+		lastLayerText.text = "Last Layer TanH (Movespeed): " + GameObject.Find ("Game Controller").GetComponent<Nodes> ().GetMoveSpeed ();
+		scoreText.text = "Score: " + GameObject.Find ("Game Controller").GetComponent<Nodes> ().score;
+		bestScore.text = "Best Score: " + GameObject.Find ("Game Controller").GetComponent<Nodes> ().oldScore;
+	}
+}
